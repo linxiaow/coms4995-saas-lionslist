929d668860901a38f1ff7588fae9f8d91f762cc0
evilo <oliviazxj@gmail.com>
2021-11-03 17:23:11 -0400

    parameters issue

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -38,20 +38,20 @@ RSpec.describe PostsController, type: :controller do
     it "post with valid parameters" do
       get :create, {:Post => {:title => "Post Title #1", :author => "Author #1",
                     :category => "furniture", :content => "I posted something."}}
-      expect(response).to redirect_to posts_path
+      expect(response).to redirect_to post_path
       expect(flash[:notice]).to match(/Post Title #1 was successfully created./)
       Post.find_by(:title => "Post Title #1").destroy
     end
   end
   
   describe "updates" do
-    it "movie's valid attributes" do
+    it "post's valid attributes" do
       post = Post.create(:title => "Post Title #2", :author => "Author #2",
                     :category => "furniture", :content => "I posted some other things.")
       get :update, {:id => post.id, :post =>
         {:content => "content changed to some other things."}
       }
-      expect(response).to redirect_to posts_path(post)
+      expect(response).to redirect_to post_path(post)
       expect(flash[:notice]).to match(/Post Title #2 was successfully updated./)
       post.destroy
     end
