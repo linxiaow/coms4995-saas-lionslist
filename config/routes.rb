c2f9c778c298232ef5e5ba1f821933a7a2648b33
Haoran Zhu <hz3fr@virginia.edu>
2021-11-16 15:08:42 -0500

    general google oauth added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rottenpotatoes::Application.routes.draw do
   resources :posts
   #get '/movies/:id/search_directors' => 'movies#show_same_director', as: :search_directors
   # map '/' to be a redirect to '/movies'
+  # Routes for Google authentication
   root :to => redirect('/posts')
+  get '/auth/:provider/callback' => 'sessions#omniauth'
+  get 'auth/failure', to: redirect('/')
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211102091826) do
+ActiveRecord::Schema.define(version: 20211116200037) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20211102091826) do
     t.string   "category"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.string "uid"
+    t.string "provider"
   end
 
 end
