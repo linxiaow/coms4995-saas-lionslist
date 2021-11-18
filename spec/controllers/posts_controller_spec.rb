630b83a23b5e5145a70166d792f4b4822be1aea3
Haoran Zhu <hz3fr@virginia.edu>
2021-11-18 00:17:24 -0500

    partial rspec test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
+require 'spec_helper'
+require 'integration_spec_helper'
 
-RSpec.describe PostsController, type: :controller do  
+RSpec.describe SessionsController, type: :request do 
+  include IntegrationSpecHelper 
   describe "creates" do
+    before(:each) do
+      login_with_oauth
+      get "/auth/google_oauth2/callback"
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    end
+
     it "post with valid parameters" do
-      get :create, {:post => {:title => "Post Title #1", :author => "Author #1",
+      post "/posts/new", {:post => {:title => "Post Title #1",
                     :category => "furniture", :content => "I posted something."}}
       expect(response).to redirect_to posts_path
+
       expect(flash[:notice]).to match(/Post Title #1 was successfully created./)
       Post.find_by(:title => "Post Title #1").destroy
     end
@@ -13,9 +23,12 @@ RSpec.describe PostsController, type: :controller do
   
   describe "updates" do
     it "post's valid attributes" do
-      post = Post.create(:title => "Post Title #2", :author => "Author #2",
+      login_with_oauth
+      get "/auth/google_oauth2/callback"
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+      post = Post.create(:title => "Post Title #2",
                     :category => "furniture", :content => "I posted some other things.")
-      get :update, {:id => post.id, :post =>
+      post "/posts/#{post.id}/edit", {:id => post.id, :post =>
         {:content => "content changed to some other things."}
       }
       expect(response).to redirect_to post_path(post)
---
+++ spec/factories/user.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+    factory :user do
+      username 'TestUser'
+      email 'testuser@gmail.com'
+      password '123'
+    end
+end
