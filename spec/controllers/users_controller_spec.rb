508de159d87e33a0064b131ba25d7db95ec67a53
DWJ01 <1521974869@qq.com>
2021-11-18 01:42:03 -0500

    Add user profile rspec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,57 +2,74 @@ require 'rails_helper'
 require 'spec_helper'
 require 'integration_spec_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe SessionsController, type: :request do
     include IntegrationSpecHelper
   
-  describe "updates" do
+  describe "user_profile:updates" do
     it "user's valid attributes" do
-      user = User.create(:username => "User1", :email => "user1@gmail.com",
-                    :password => "123")
-      get :update, {:id => user.id, :user =>
+        login_with_oauth
+        get "/auth/google_oauth2/callback"
+        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+        put "/profile", {:user =>
         {:phone => "123456"}
-      }
-      expect(response).to redirect_to profile_path
-      expect(flash[:notice]).to match(/User1's personal profile was successfully updated./)
-      user.destroy
+        }
+        expect(response).to redirect_to profile_path
+        expect(flash[:notice]).to match(/'s personal profile was successfully updated./)
     end
   end
 
-  describe 'edit' do
+describe 'user_profile:edit' do
+    before do
+        login_with_oauth
+        get "/auth/google_oauth2/callback"
+        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+        get "/edit/profile"
+    end
+
+    it 'finds the user' do
+        expect(assigns(:user)).not_to be nil
+    end
+
     it 'renders the edit template' do
-      get :edit
-      expect(response).to render_template('edit')
+        expect(response).to render_template('edit')
     end
-  end
+end
   
-  describe 'show' do
-    user = FactoryGirl.create(:user)
+describe 'user_profile:show' do
     before(:each) do
-      get :show
+        login_with_oauth
+        get "/auth/google_oauth2/callback"
+        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+        get "/profile"
     end
 
-    it 'finds the user' do
-      expect(assigns(:user)).to eql(user)
+    it 'finds the user and posts' do
+        expect(assigns(:user)).not_to be nil
+        expect(assigns(:posts)).not_to be nil
     end
 
     it 'renders the show template' do
-      expect(response).to render_template('show')
+        expect(response).to render_template('show')
     end
-  end
+end
 
-  describe 'show_other' do
+describe 'user_profile:show_other' do
     user = FactoryGirl.create(:user)
     before(:each) do
-      get :show, id: user.id
+        login_with_oauth
+        get "/auth/google_oauth2/callback"
+        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+        get "/profile/#{user.id}"
     end
 
-    it 'finds the user' do
-      expect(assigns(:user)).to eql(user)
+    it 'finds the user and posts' do
+        expect(assigns(:user)).to eql(user)
+        expect(assigns(:posts)).not_to be nil
     end
 
     it 'renders the show template' do
-      expect(response).to render_template('show_other')
+        expect(response).to render_template('show_other')
     end
-  end
+end
 
 end
