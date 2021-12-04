f9be41bc4d021bc741b4ebd2a6b5704d2b84e1da
Haoran Zhu <hz3fr@virginia.edu>
2021-12-03 21:39:18 -0500

    add user seeds & minor error fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+users = [{:id => '997', :username => 'testauthor1', :email => 'testauthor1@columbia.edu', :password => 'MyDummyPassword1'},
+		{:id => '998', :username => 'testauthor2', :email => 'testauthor2@columbia.edu', :password => 'MyDummyPassword2'},
+		{:id => '999', :username => 'testauthor3', :email => 'testauthor3@columbia.edu', :password => 'MyDummyPassword3'}]
 
-posts = [{:title => 'Test1', :content => 'AAA', :author => 'testauthor1', :author_id => '1', :category => 'furniture'},
-    	  {:title => 'Test2', :content => 'BBB', :author => 'testauthor2', :author_id => '1', :category => 'sublease'},
-    	  {:title => 'Test3', :content => 'CCC', :author => 'testauthor3', :author_id => '2', :category => 'furniture'},
+posts = [{:title => 'Test1', :content => 'AAA', :author => 'testauthor1', :author_id => '997', :category => 'furniture'},
+    	  {:title => 'Test2', :content => 'BBB', :author => 'testauthor2', :author_id => '998', :category => 'sublease'},
+    	  {:title => 'Test3', :content => 'CCC', :author => 'testauthor3', :author_id => '999', :category => 'furniture'},
   	 ]
 
 posts.each do |post|
 	Post.create!(post)
+end
+
+users.each do |user|
+	User.create!(user)
 end
