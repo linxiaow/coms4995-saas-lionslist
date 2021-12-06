134bed710acbd40bb8c0d921cca68948ef57d11d
Linxiao Wu <linxiaow@umich.edu>
2021-12-06 16:42:17 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-users = [{:id => '-1', :username => 'testauthor1', :email => 'testauthor1@columbia.edu', :password => 'MyDummyPassword1'},
-		{:id => '-2', :username => 'testauthor2', :email => 'testauthor2@columbia.edu', :password => 'MyDummyPassword2'},
-		{:id => '-3', :username => 'testauthor3', :email => 'testauthor3@columbia.edu', :password => 'MyDummyPassword3'}]
+users = [{:id => '-1', :username => 'John Smith', :email => 'testauthor1@columbia.edu', :password => 'MyDummyPassword1'},
+		{:id => '-2', :username => 'Teddy Bear', :email => 'testauthor2@columbia.edu', :password => 'MyDummyPassword2'},
+		{:id => '-3', :username => 'Foo Bar', :email => 'testauthor3@columbia.edu', :password => 'MyDummyPassword3'}]
 
-posts = [{:title => 'Test1', :content => 'AAA', :author => 'testauthor1', :author_id => '-1', :category => 'furniture'},
-    	  {:title => 'Test2', :content => 'BBB', :author => 'testauthor2', :author_id => '-2', :category => 'sublease'},
-    	  {:title => 'Test3', :content => 'CCC', :author => 'testauthor3', :author_id => '-3', :category => 'furniture'},
+posts = [{:title => 'Second Hand Sofa', :content => '1 year usage, good condition', :author => 'John Smith', :author_id => '-1', :category => 'furniture'},
+    	  {:title => 'Apartment @ Enclave', :content => 'Master bedroom, only 2000 a month!', :author => 'Teddy Bear', :author_id => '-2', :category => 'sublease'},
+    	  {:title => 'TV', :content => "I don't want it because it is too large. Contact me if you want it", :author => 'Foo Bar', :author_id => '-3', :category => 'furniture'},
   	 ]
 
 posts.each do |post|
