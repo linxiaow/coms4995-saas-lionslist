2d72c537b0afa69262a8d92049c0cb0918c96f45
Haoran Zhu <hz3fr@virginia.edu>
2021-12-06 14:52:31 -0500

    edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-users = [{:id => '997', :username => 'testauthor1', :email => 'testauthor1@columbia.edu', :password => 'MyDummyPassword1'},
-		{:id => '998', :username => 'testauthor2', :email => 'testauthor2@columbia.edu', :password => 'MyDummyPassword2'},
-		{:id => '999', :username => 'testauthor3', :email => 'testauthor3@columbia.edu', :password => 'MyDummyPassword3'}]
+users = [{:id => '-1', :username => 'testauthor1', :email => 'testauthor1@columbia.edu', :password => 'MyDummyPassword1'},
+		{:id => '-2', :username => 'testauthor2', :email => 'testauthor2@columbia.edu', :password => 'MyDummyPassword2'},
+		{:id => '-3', :username => 'testauthor3', :email => 'testauthor3@columbia.edu', :password => 'MyDummyPassword3'}]
 
-posts = [{:title => 'Test1', :content => 'AAA', :author => 'testauthor1', :author_id => '997', :category => 'furniture'},
-    	  {:title => 'Test2', :content => 'BBB', :author => 'testauthor2', :author_id => '998', :category => 'sublease'},
-    	  {:title => 'Test3', :content => 'CCC', :author => 'testauthor3', :author_id => '999', :category => 'furniture'},
+posts = [{:title => 'Test1', :content => 'AAA', :author => 'testauthor1', :author_id => '-1', :category => 'furniture'},
+    	  {:title => 'Test2', :content => 'BBB', :author => 'testauthor2', :author_id => '-2', :category => 'sublease'},
+    	  {:title => 'Test3', :content => 'CCC', :author => 'testauthor3', :author_id => '-3', :category => 'furniture'},
   	 ]
 
 posts.each do |post|
