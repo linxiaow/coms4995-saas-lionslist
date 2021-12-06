76ce983a71b396d1a1ccade5ec32e7a90c7cfd71
Haoran Zhu <hz3fr@virginia.edu>
2021-12-06 16:50:14 -0500

    heroku deploy #13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,19 @@ class UsersController < ApplicationController
         @user = User.find(session[:user_id])
         @current_user_id = @user.id
         @posts = Post.search_author(session[:user_id])
-        @purchase_deals = Deal.search_purchase_requests(session[:user_id])
+        @purchase_deals = Deal.search_purchase_requests(session[:user_id].to_s)
         puts "hit1"
         puts @purchase_deals
-        @selling_deals = Deal.search_selling_requests(session[:user_id])
+        @selling_deals = Deal.search_selling_requests(session[:user_id].to_s)
         puts "hit2"
         puts @selling_deals
-        @unrated_deals = Deal.search_unrated_deal(session[:user_id])
+        @unrated_deals = Deal.search_unrated_deal(session[:user_id].to_s)
         puts "hit3"
         puts @unrated_deals
-        @involved_deals = Deal.search_involved_deal(session[:user_id])
+        @involved_deals = Deal.search_involved_deal(session[:user_id].to_s)
         puts "hit4"
         puts @involved_deals
-        @user_rating = Deal.calcualte_rating(session[:user_id])
+        @user_rating = Deal.calcualte_rating(session[:user_id].to_s)
         puts "hit5"
         puts @involved_deals
         render "show"
