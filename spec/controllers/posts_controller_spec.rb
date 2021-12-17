3f91a9fbd0d3efa4a6ed105039b8a6fc05e4610d
DWJ01 <1521974869@qq.com>
2021-12-17 17:47:35 -0500

    Add rspec tests for filter and sort

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -55,13 +55,100 @@ RSpec.describe SessionsController, type: :request do
 
 
   describe 'index' do
-    it 'renders the index template' do
+    before(:each) do
       login_with_oauth
       get "/auth/google_oauth2/callback"
       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    end
+
+    it 'renders the index template' do
       get '/posts'
       expect(response).to render_template('index')
     end
+
+    it 'filter posts by attributes' do
+      post1 = Post.create(:title => "Post Title #1",
+        :category => "furniture", :content => "I posted some things.")
+      post2 = Post.create(:title => "Post Title #2",
+          :category => "sublease", :content => "I posted some other things.")
+      post1.author_id = 1
+      post1.save
+      post2.author_id = 1
+      post2.save
+      get '/posts?category=furniture&author=&title='
+      expect(response.body).to match(/Post Title #1/)
+      post1.destroy
+      post2.destroy
+    end
+
+    it 'sort posts by author' do
+      post1 = Post.create(:title => "Post Title #1",
+        :category => "furniture", :content => "I posted some things.")
+      post2 = Post.create(:title => "Post Title #2",
+          :category => "sublease", :content => "I posted some other things.")
+      post1.author_id = 1
+      post1.save
+      post2.author_id = 1
+      post2.save
+      get '/posts?sort_by=author'
+      index1 = response.body.index(/Post Title #1/)
+      index2 = response.body.index(/Post Title #2/)
+      expect(index1).to be < index2
+      post1.destroy
+      post2.destroy
+    end
+
+    it 'sort posts by category' do
+      post1 = Post.create(:title => "Post Title #1",
+        :category => "furniture", :content => "I posted some things.")
+      post2 = Post.create(:title => "Post Title #2",
+          :category => "sublease", :content => "I posted some other things.")
+      post1.author_id = 1
+      post1.save
+      post2.author_id = 1
+      post2.save
+      get '/posts?sort_by=category'
+      index1 = response.body.index(/Post Title #1/)
+      index2 = response.body.index(/Post Title #2/)
+      expect(index1).to be < index2
+      post1.destroy
+      post2.destroy
+    end
+
+    it 'sort posts by title' do
+      post1 = Post.create(:title => "Post Title #1",
+        :category => "furniture", :content => "I posted some things.")
+      post2 = Post.create(:title => "Post Title #2",
+          :category => "sublease", :content => "I posted some other things.")
+      post1.author_id = 1
+      post1.save
+      post2.author_id = 1
+      post2.save
+      get '/posts?sort_by=title'
+      index1 = response.body.index(/Post Title #1/)
+      index2 = response.body.index(/Post Title #2/)
+      expect(index1).to be < index2
+      post1.destroy
+      post2.destroy
+    end
+
+    it 'sort posts by created date' do
+      post1 = Post.create(:title => "Post Title #1",
+        :category => "furniture", :content => "I posted some things.")
+      post2 = Post.create(:title => "Post Title #2",
+          :category => "sublease", :content => "I posted some other things.")
+      post1.author_id = 1
+      post1.save
+      post2.author_id = 1
+      post2.save
+      get '/posts?sort_by=created+date'
+      index1 = response.body.index(/Post Title #1/)
+      index2 = response.body.index(/Post Title #2/)
+      expect(index1).to be > index2
+      post1.destroy
+      post2.destroy
+    end
+
   end
   
   describe 'show' do
@@ -70,6 +157,7 @@ RSpec.describe SessionsController, type: :request do
       get "/auth/google_oauth2/callback"
       request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
     end
+
     it 'shows the post' do
       post = Post.create(:title => "Post Title #2",
         :category => "furniture", :content => "I posted some other things.")
@@ -90,6 +178,23 @@ RSpec.describe SessionsController, type: :request do
     # it 'renders the show template' do
     #   expect(response).to render_template('show')
     # end
+  end
+
+  describe 'edit' do
+    before(:each) do
+      login_with_oauth
+      get "/auth/google_oauth2/callback"
+      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    end
+
+    it 'enter edit page' do
+      post = Post.create(:title => "Post Title #1",
+        :category => "furniture", :content => "I posted some things.")
+      post.author_id = 1
+      post.save
+      get "/posts/#{post.id}/edit"
+      expect(assigns(:post)).to eql(post)
+    end
   end
 
 end
